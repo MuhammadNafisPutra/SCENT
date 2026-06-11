731b14a663ba05575165dcb4c37aa69900b4d3da
Muhammad Ghazi Rakhmadi <mghazirakhmadi63@gmail.com>
2026-06-11 19:14:05 +0800

    Delete scent 11 jt/scent-fixed/app/src/main directory

--- scent 11 jt/scent-fixed/app/src/main/java/com/contoh/scentapp/data/model/ProfileUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.contoh.scentapp.data.model
-
-data class ProfileUiState(
-    val name       : String  = "Nama Kamu",
-    val email      : String  = "nafisnervers@gmail.com",
-    val isDarkMode : Boolean = true,
-    val language   : String  = "INDONESIA",
-    val showDeleteDialog : Boolean = false
-)
